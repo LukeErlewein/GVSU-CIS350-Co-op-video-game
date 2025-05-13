42fd8f86a1faaf2c36f3bc371e7f6f4d22e00642
QuentinD <quentindaniere@gmail.com>
2025-05-13 01:31:58 -0400

    Basic Bullets

---
+++ src/scripts/Bullet.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var attack: Attack
+var direction:= Vector2.ZERO
+
+func _process(delta: float) -> void:
+	if direction != Vector2.ZERO:
+		var velocity = direction * attack.bullet_speed
+		
+		global_position += velocity
+
+
+
+func set_direction(direction: Vector2):
+	self.direction = direction
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("damage"):
+		body.damage(attack)
+		queue_free()
