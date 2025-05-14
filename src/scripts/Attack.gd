5f832e92295045ec71250cda33ecea52ac63df86
DaniereQ <152230132+DaniereQ@users.noreply.github.com>
2025-05-14 14:54:49 -0400

    Feature/RangerPlayerandEnergyCells

--- src/scripts/Attack.gd
+++ src/scripts/Attack.gd
@@ -5,3 +5,4 @@ var knockback_force: float = 100.0
 var attack_position: Vector2
 var bullet_speed: float = 40.0
 var hurt_cell_holders: bool = false
+var attack_cooldown: float = 0.5 # time between attacks
