f90389db094ab428e9d84905e051e65f2773bf4b
QuentinD <quentindaniere@gmail.com>
2025-05-16 17:32:03 -0400

    Grunt Art, Working on navigation AI

--- src/scripts/enemyscripts/BaseEnemy.gd
+++ src/scripts/enemyscripts/BaseEnemy.gd
@@ -3,5 +3,11 @@ class_name BaseEnemy extends CharacterBody2D
 func attack():
 	pass
 
-func set_target():
-	pass
+# Get Node from scene tree, whether that is core or players
+func set_target(stats):
+	if stats.target == 0:
+		pass
+	elif stats.target == 1:
+		pass
+	elif stats.target == 2:
+		pass
--- src/scripts/enemyscripts/Grunt.gd
+++ src/scripts/enemyscripts/Grunt.gd
@@ -1,3 +1,26 @@
-extends BaseEnemy
+class_name Grunt extends BaseEnemy
 
 @export var stats : EnemyStats
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@export var target : Node2D
+
+func _ready() -> void:
+	health_component.MAX_HEALTH = stats.health
+	sprite_2d.texture = stats.texture
+	sprite_2d.rotate(-PI * 0.5)
+	#set_target(stats.target)
+	
+
+func _physics_process(delta: float) -> void:
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * stats.speed
+	move_and_slide()
+
+func make_path() -> void:
+	nav_agent.target_position = target.global_position
+
+func _on_debug_path_timeout() -> void:
+	make_path()
