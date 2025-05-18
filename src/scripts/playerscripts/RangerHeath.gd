e8e20213ef9e749cda0a077bd64aa747389a3a30
DESKTOP-U7P00EV\luket <lukeerlewein@gmail.com>
2025-05-18 17:58:29 -0400

    Adds fade in/fade out to health

--- src/scripts/playerscripts/RangerHeath.gd
+++ src/scripts/playerscripts/RangerHeath.gd
@@ -5,22 +5,37 @@ var player: Node2D
 var health_component
 
 @onready var health_bar = $HealthBar
+@onready var health_label = $HealthLabel
+@onready var animation_player = $AnimationPlayer
+
+var is_visible := true
 
 func _ready():
 	player = get_node(player_node_path)
 	health_component = player.get_node("HealthComponent")
 	health_bar.max_value = health_component.MAX_HEALTH
-
+	modulate.a = 0.0  # Start hidden
+	is_visible = false
 
 func _process(_delta):
-	if player == null:
+	if player == null or health_component == null:
 		return
 
+	# Position above player
 	var camera := get_viewport().get_camera_2d()
 	var half_screen: Vector2 = get_viewport().get_visible_rect().size / 2.0
 	var screen_pos: Vector2 = player.global_position - camera.global_position + half_screen
-	global_position = screen_pos + Vector2(-37, -40)  # offset above player
+	global_position = screen_pos + Vector2(-37, -40)
 
 	var current: float = health_component.health
 	health_bar.value = current
-	$HealthLabel.text = str(round(current)) + "/" + str(round(health_component.MAX_HEALTH))
+	health_label.text = str(round(current)) + "/" + str(round(health_component.MAX_HEALTH))
+
+	var should_be_visible = current < health_component.MAX_HEALTH
+
+	if should_be_visible and not is_visible:
+		animation_player.play("FadeIn")
+		is_visible = true
+	elif not should_be_visible and is_visible:
+		animation_player.play("FadeOut")
+		is_visible = false
